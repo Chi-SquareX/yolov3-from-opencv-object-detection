33010ffb49b8352336121d2a1898e8e7e9241245
Rahul Golder <65487329+Goldenprince8420@users.noreply.github.com>
2023-01-22 17:19:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ for img_name in os.listdir(os.path.join(".", input_dir)):
         #             7,
         #             (0, 255, 0),
         #             15)
-        license_plate = img[int(yc - (h / 2)):int(yc + (h / 2)), int(xc - (w / 2)):int(xc + (w / 2)), :]
+        license_plate = img[int(yc - (h / 2)):int(yc + (h / 2)), int(xc - (w / 2)):int(xc + (w / 2)), :].copy()
         img = cv2.rectangle(img,
                             (int(xc - (w / 2)), int(yc - (h / 2))),
                             (int(xc + (w / 2)), int(yc + (h / 2))),
